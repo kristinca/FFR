d77507bdbb51a77f22b43a5333af097d6e763830
Kristina Jovanovska <72104951+kristinca@users.noreply.github.com>
2022-02-06 23:24:22 +0100

    up_down working ok

--- 1dd.py
+++ 1dd.py
@@ -55,7 +55,8 @@ def t_start():
     :return: list with the time interval needed for the sample to reach the bottom from initial time = 0
     """
     return [i for i in range(0, 25)]
-
+ii = []
+jj = []
 
 def going_down_first_time(i, j):
     td = t_start()
@@ -78,7 +79,7 @@ def going_up(n, ii,jj):
         H.append((max(Np2[:, 1]) - min(Np2[:, 1])) / np.average(Np2[:, 1]))
         ii -= 1
         jj -= 1
-    return n, ii, jj, Np2, H
+    return ii, jj, Np2, H
 
 
 def going_down(n, ii, jj):
@@ -91,16 +92,32 @@ def going_down(n, ii, jj):
         H.append((max(Np2[:, 1]) - min(Np2[:, 1])) / np.average(Np2[:, 1]))
         ii += 1
         jj += 1
-    return n2, ii, jj, Np2, H
+    return ii, jj, Np2, H
 
 
-i1, j1, Np1, H1= going_down_first_time(25, 45)
-n2, i2, j2, Np22, H2 = going_up(0, i1, j1)
-n3, i3, j3, Np3, H3 = going_down(n2, i2, j2)
+def up_down(n_times):
+    for n in range(0, n_times):
+        iii1, ji1, Npi1, Hi1 = going_up(n, iii[-1], jjj[-1])
+        iii.append(iii1)
+        jjj.append(ji1)
+        iii2, ji2, Npi2, Hi2 = going_down(n, iii[-1], jjj[-1])
+        iii.append(iii2)
+        jjj.append(ji2)
+    return Npi2, Hi2
+#
+# i2, j2, Np22, H2 = going_up(0, i1, j1)
+# i3, j3, Np3, H3 = going_down(0, i2, j2)
+# i4, j4, Np4, H4 = going_up(1, i3, j3)
+# i5, j5, Np5, H5 = going_down(1, i4, j4)
+# i6, j6, Np6, H6 = going_up(2, i5, j5)
+# i5, j7, Np7, H7 = going_down(2, i6, j6)
+i1, j1, Np1, H1 = going_down_first_time(25, 45)
+iii = [i1]
+jjj = [j1]
+N6, H6 = up_down(5)
 
-
-
-plt.plot(H3, '-')
+# plt.plot(N6[:, 1], '-')
+plt.plot(H6, '-')
 plt.title(f'H = f (t)')
 plt.xlabel('time [s]')
 plt.ylabel('H [/]')
