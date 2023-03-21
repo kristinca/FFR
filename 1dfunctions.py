230cbbab7bb1b550854cca8b7e6f9327d7cb27b0
Kristina Jovanovska <kristinajovanovska993@gmail.com>
2023-03-21 23:41:44 +0100

    refactored other functions - part 1, removed global vars

--- 1dfunctions.py
+++ 1dfunctions.py
@@ -3,96 +3,100 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def get_flux_1d():
+def get_flux_1d() -> tuple:
     """
-    :return: The reactor flux in 1D
+    Function that calculates the reactor flux in 1D
+
+    :return: The reactor flux in 1D, Sigma(Si-30)
     """
     # hr = 70 [cm]
     h = 70
     # split the y axis into 70 elements, 1 cm each
     y = [elem for elem in range(-35, 36)]
     # Sigma(Si-30) = sigma(Si-30)(n,gamma)*N(Si-30)
-    sigma_si_30_micro = 107.2 * 10**(-27)
+    sigma_si_30_micro = 107.2 * 10 ** (-27)
     # n_si_30 = (roSi30*Na)/MSi30
     # rho(Si-30) = 2.33 [g/cm^3]
     rho_si_30 = 2.33
-    na = 6.022*math.pow(10, 23)
+    na = 6.022 * math.pow(10, 23)
     # M(Si-30) = 29.973770 [g/mol]
     molar_mass_si_30 = 29.973770
-    n_si_30 = (rho_si_30*na)/molar_mass_si_30
-    sigma_si_30 = sigma_si_30_micro*n_si_30
+    n_si_30 = (rho_si_30 * na) / molar_mass_si_30
+    sigma_si_30 = sigma_si_30_micro * n_si_30
     print(f'SigmaSi30 = {sigma_si_30}, NSi30= {n_si_30}')
     # initial flux = f0 = 10^13 [1/s*cm^2]
     f0 = math.pow(10, 13)
-    flux_1d = [f0*(math.cos((math.pi*element)/70)) for element in y]
+    flux_1d = [f0 * (math.cos((math.pi * element) / 70)) for element in y]
     return flux_1d, sigma_si_30
 
 
-# get the flux in 1D and SigmaSi30
-f, sSi30 = get_flux_1d()
-
-# plt.plot(f)
-# plt.show()
-
-# initialize P-31 number density, homogeneity
-np2_ = np.zeros((20, 2))
-h_ = []
-
-
-def t_up(n_up):
+def t_up(
+        n_up: int
+) -> list:
     """
+    Function that returns list with the time interval when the sample is going up
+
     :param n_up: n-th number of time the sample goes up
     :return: list - the time interval needed for the sample to reach the top with dt=1[s], v=1[cm/s]
     """
-    return [tup for tup in range(25+100*n_up, 75+100*n_up)]
+    return [tup for tup in range(25 + 100 * n_up, 75 + 100 * n_up)]
 
 
-def t_down(n_down):
+def t_down(
+        n_down: int
+) -> list:
     """
+    Function that returns list with the time interval when the sample is going down
+
     :param n_down: n-th number of time the sample goes down
     :return: list - the time interval needed for the sample to reach the bottom with dt=1[s], v=1[cm/s]
     """
-    return [tdd for tdd in range(75+100*n_down, 125+100*n_down)]
+    return [tdd for tdd in range(75 + 100 * n_down, 125 + 100 * n_down)]
 
 
-def t_start():
+def t_start() -> list:
     """
+    Function that returns list with the time interval when the sample is going down for the first time
+
     :return: list - the time interval needed for the sample to reach the bottom from initial t=0[s] with v=1[cm/s]
     """
     return [ts for ts in range(0, 25)]
 
 
-# initialize ii-list with highest index position of the sample and jj-list with lowest index position of the sample
-ii = []
-jj = []
-
-
-def going_down_first_time(ift, jft):
+def going_down_first_time(ift, jft, reactor_flux, sigmasi30):
     """
-    A function that calculates from initial time till the Si-30 sample reaches the bottom.
+    A function that calculates from initial time till the Si-30 sample reaches the bottom
+
     :param ift: highest index of the Si-30 sample on the initial flux indexes scale
     :param jft: lowest index of the Si-30 sample on the initial flux indexes scale
+    :param reactor_flux: list, the reactor flux
+    :param sigmasi30: float, the Sigma(Si-30)
     :return: highest and lowest index of the Si-30 sample on the initial flux indexes scale,
              P-31 number density, homogeneity
     """
+    h_0 = []
     td = t_start()
     for el1 in td:
         print(f't={el1} [s], i={ift}, j={jft}')
         np2_[:, 0] = [el1 for i in range(20)]
-        np2_[:, 1] = np2_[:, 1] + [sSi30 * el1 * m for m in f[ift:jft]]
+        np2_[:, 1] = np2_[:, 1] + [sigmasi30 * el1 * m for m in reactor_flux[ift:jft]]
         if el1 != 0:
-            h_.append((max(np2_[:, 1]) - min(np2_[:, 1])) / np.average(np2_[:, 1]))
+            h_0.append((max(np2_[:, 1]) - min(np2_[:, 1])) / np.average(np2_[:, 1]))
         ift += 1
         jft += 1
-    return ift, jft, np2_, h_
+    return ift, jft, np2_, h_0
 
 
-def going_up(n, ii, jj):
+def going_up(n, ii, jj, reactor_flux, sigmasi30, np_up, h_up):
     """
     A function that calculates from initial time till the Si-30 sample reaches the top from the bottom.
     :param n: the n-th time of going  up
     :param ii: highest index of the Si-30 sample on the initial flux indexes scale
     :param jj: lowest index of the Si-30 sample on the initial flux indexes scale
+    :param reactor_flux: list, the reactor flux
+    :param sigmasi30: float, the Sigma(Si-30)
+    :param np_up: P-31 number density
+    :param h_up: homogeneity
     :return: highest and lowest index of the Si-30 sample on the initial flux indexes scale,
              P-31 number density, homogeneity
     """
@@ -100,20 +104,24 @@ def going_up(n, ii, jj):
     tu = t_up(n)
     for el2 in tu:
         print(f't={el2} [s], i={ii}, j={jj}')
-        np2_[:, 0] = [el2 for i in range(20)]
-        np2_[:, 1] = np2_[:, 1] + [sSi30 * el2 * m for m in f[ii:jj]]
-        h_.append((max(np2_[:, 1]) - min(np2_[:, 1])) / np.average(np2_[:, 1]))
+        np_up[:, 0] = [el2 for i in range(20)]
+        np_up[:, 1] = np_up[:, 1] + [sigmasi30 * el2 * m for m in reactor_flux[ii:jj]]
+        h_up.append((max(np_up[:, 1]) - min(np_up[:, 1])) / np.average(np_up[:, 1]))
         ii -= 1
         jj -= 1
-    return ii, jj, np2_, h_
+    return ii, jj, np_up, h_up
 
 
-def going_down(n, ii, jj):
+def going_down(n, ii, jj, reactor_flux, sigmasi30, np_down, h_down):
     """
     A function that calculates from initial time till the Si-30 sample reaches the bottom from the top.
     :param n: the n-th time of going down
     :param ii: highest index of the Si-30 sample on the initial flux indexes scale
     :param jj: lowest index of the Si-30 sample on the initial flux indexes scale
+    :param reactor_flux: list, the reactor flux
+    :param sigmasi30: float, the Sigma(Si-30)
+    :param np_down: P-31 number density
+    :param h_down: homogeneity
     :return: highest and lowest index of the Si-30 sample on the initial flux indexes scale,
              P-31 number density, homogeneity
     """
@@ -121,41 +129,49 @@ def going_down(n, ii, jj):
     td = t_down(n)
     for el1 in td:
         print(f't={el1} [s], i={ii}, j={jj}')
-        np2_[:, 0] = [el1 for i in range(20)]
-        np2_[:, 1] = np2_[:, 1] + [sSi30 * el1 * m for m in f[ii:jj]]
-        h_.append((max(np2_[:, 1]) - min(np2_[:, 1])) / np.average(np2_[:, 1]))
+        np_down[:, 0] = [el1 for i in range(20)]
+        np_down[:, 1] = np_down[:, 1] + [sigmasi30 * el1 * m for m in reactor_flux[ii:jj]]
+        h_down.append((max(np_down[:, 1]) - min(np_down[:, 1])) / np.average(np2_[:, 1]))
         ii += 1
         jj += 1
-    return ii, jj, np2_, h_
+    return ii, jj, np_down, h_down
 
 
-def up_down(n_times):
+def up_down(n_times, np31, hom1, reactor_flux, sigmasi30):
     """
     The driver function.
-    :param n_times: Number of times of going up and down of the Si-30 sample.
+
+    :param n_times: int, number of times of going up and down of the Si-30 sample
+    :param reactor_flux: list, the reactor flux
+    :param sigmasi30: float, the Sigma(Si-30)
     :return: P-31 number density, homogeneity
     """
+    np32 = []
     for n in range(0, n_times):
-        iii1, ji1, npi1_, hi1_ = going_up(n, iii[-1], jjj[-1])
+        iii1, ji1, npi1_, hi1_ = going_up(n, iii[-1], jjj[-1], reactor_flux, sigmasi30, np31, hom1)
         iii.append(iii1)
         jjj.append(ji1)
-        iii2, ji2, npi2_, hi2_ = going_down(n, iii[-1], jjj[-1])
+        iii2, ji2, np32, hom1 = going_down(n, iii[-1], jjj[-1], reactor_flux, sigmasi30, npi1_, hi1_)
         iii.append(iii2)
         jjj.append(ji2)
-    return npi2_, hi2_
+    return np32, hom1
 
 
 if __name__ == '__main__':
-    i1, j1, np1_, h1_ = going_down_first_time(25, 45)
+    # get the flux in 1D and SigmaSi30
+    f, sigma_si30 = get_flux_1d()
+    # initialize P-31 number density, homogeneity
+    np2_ = np.zeros((20, 2))
+    i1, j1, np1_, h1_ = going_down_first_time(25, 45, f, sigma_si30)
     iii = [i1]
     jjj = [j1]
     # going up and down 5 times
     num = 50
-    n, h_ = up_down(num)
+    np31, hom = up_down(num, np1_, h1_, reactor_flux=f, sigmasi30=sigma_si30)
 
     # plot Np31 = f(h)
 
-    plt.plot(n[:, 1]/10**13, '-', color='purple')
+    plt.plot(np31[:, 1] / 10 ** 13, '-', color='purple')
     plt.title('$\mathregular{N_{P-31 }}$ = f (h)')
     plt.xlabel('h')
     plt.ylabel('$\mathregular{N_{P-31} [10^{13}/cm^3]}$')
@@ -167,7 +183,7 @@ if __name__ == '__main__':
 
     # plot H = f(t)
 
-    plt.plot(h_, '-')
+    plt.plot(hom, '-')
     plt.title(f'H = f (t)')
     plt.xlabel('time [s]')
     plt.ylabel('H [/]')
